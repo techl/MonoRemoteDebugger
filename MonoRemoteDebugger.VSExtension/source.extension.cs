61f3e7cf62ea8f9fe4fd60022d5a93701c54c9a4
Bongho Lee <techcap@live.com>
2016-04-26 17:07:41 +0900

    v1.0.5

--- MonoRemoteDebugger.VSExtension/source.extension.cs
+++ MonoRemoteDebugger.VSExtension/source.extension.cs
@@ -6,7 +6,7 @@ namespace MonoRemoteDebugger.VSExtension
         public const string Name = "MonoRemoteDebugger";
         public const string Description = "MonoRemoteDebugger";
         public const string Language = "en-US";
-        public const string Version = "1.0";
+        public const string Version = "1.0.5";
         public const string Author = "Techl.com";
         public const string Tags = "mono, debugger, remote";
     }
